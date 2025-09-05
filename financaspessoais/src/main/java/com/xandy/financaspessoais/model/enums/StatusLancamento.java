56750053755b5a256b729f160f929781fd7ccaff
XandyGomes <x3.computadores@hotmail.com>
2025-09-05 19:31:07 -0300

    Aula.05.09.2025

---
+++ financaspessoais/src/main/java/com/xandy/financaspessoais/model/enums/StatusLancamento.java
@@ -0,0 +1,9 @@
+package com.xandy.financaspessoais.model.enums;
+
+public enum StatusLancamento {
+	
+	PENDENTE,
+	CANCELADO,
+	EFETIVADO
+
+}
---
+++ financaspessoais/src/main/java/com/xandy/financaspessoais/model/enums/TipoLancamento.java
@@ -0,0 +1,8 @@
+package com.xandy.financaspessoais.model.enums;
+
+public enum TipoLancamento {
+	
+	RECEITA,
+	DESPESA
+
+}
